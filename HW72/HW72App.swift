faa483104f661d638a2f3048bed9822965c0cfdd
Abinaya Dinesh <abinayadinesh1@gmail.com>
2023-03-21 16:27:47 -0700

    Initial Commit

---
+++ HW72/HW72App.swift
@@ -0,0 +1,17 @@
+//
+//  HW72App.swift
+//  HW72
+//
+//  Created by Abinaya on 3/21/23.
+//
+
+import SwiftUI
+
+@main
+struct HW72App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
